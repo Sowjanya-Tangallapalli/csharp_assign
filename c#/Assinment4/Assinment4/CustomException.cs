34a740a98ef5c8520955b07dd39443ab242ee879
Sowjanya-Tangallapalli <102534038+Sowjanya-Tangallapalli@users.noreply.github.com>
2022-04-21 08:33:43 +0530

    Add files via upload

---
+++ c#/Assinment4/Assinment4/CustomException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assinment4
+{
+    class StackException : Exception
+    {
+        public StackException(String Message) : base(Message)
+        {
+
+        }
+    }
+
+
+
+    internal class CustomException
+    {
+
+    }
+
+
+
+
+
+}
