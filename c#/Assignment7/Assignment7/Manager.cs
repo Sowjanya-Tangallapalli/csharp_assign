e1296129201ecf95474d1aa2aed055d59ead5d38
Sowjanya-Tangallapalli <102534038+Sowjanya-Tangallapalli@users.noreply.github.com>
2022-04-21 08:41:17 +0530

    Add files via upload

---
+++ c#/Assignment7/Assignment7/Manager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment7
+{
+    [Serializable]
+    internal class Manager
+    {
+        public  int EID;
+        public  String Name;
+        public float Salary;
+        static void Main(string[] args)
+        {
+            Manager mobj = new Manager();
+            mobj.EID= 1;
+            mobj.Name = "john";
+            mobj.Salary = 25000;
+
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(@"E:\Sample2.txt", FileMode.Create, FileAccess.Write);
+
+            formatter.Serialize(stream, mobj);
+            stream.Close();
+
+            stream = new FileStream(@"E:\Sample2.txt", FileMode.Open, FileAccess.Read);
+            Manager objnew = (Manager)formatter.Deserialize(stream);
+
+            Console.WriteLine(objnew.EID);
+            Console.WriteLine(objnew.Name);
+            Console.WriteLine(objnew.Salary);
+
+
+            Console.ReadKey();
+        }
+    }
+}
