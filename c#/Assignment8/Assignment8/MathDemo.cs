8c4ca601e11decacf994f29ffa2b51b5315b926c
Sowjanya-Tangallapalli <102534038+Sowjanya-Tangallapalli@users.noreply.github.com>
2022-04-21 08:41:45 +0530

    Add files via upload

---
+++ c#/Assignment8/Assignment8/MathDemo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment8
+{
+    internal class MathDemo
+    {
+      static void main() { 
+            int a = 5;
+            public  void cube()
+            {
+                int b = a * a * a;
+                Console.WriteLine("Cube of a number is:" + b);
+            }
+        }
+    }
+}
