58919a63bdc7174e07d3a322fa0e9035022e6005
Sowjanya-Tangallapalli <102534038+Sowjanya-Tangallapalli@users.noreply.github.com>
2022-04-21 08:36:57 +0530

    Add files via upload

---
+++ c#/Assignment5/Assignment5/ArrayListSample.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int EmpSal { get; set; }
+        public string EmpLoc { get; set; }
+
+
+    }
+
+        internal class ArrayListSample
+        {
+            static void Main(string[] args)
+            {
+                //object initilizer or collection initilizer
+                //ArrayList e = new ArrayList();//non generic
+               
+                ArrayList e = new ArrayList()
+                   {
+new Employee{EmpId=1,EmpName="nick",EmpSal=10000,EmpLoc="Hyd"},
+new Employee{EmpId=2,EmpName="john",EmpSal=24000,EmpLoc="Hyd"},
+new Employee{EmpId=3,EmpName="faf",EmpSal=21000,EmpLoc="Hyd"},
+new Employee{EmpId=3,EmpName="fasil",EmpSal=30000,EmpLoc="Hyd"},
+new Employee{ EmpId=4, EmpName="fahadh"},
+
+
+};
+                foreach (Employee i in e)
+                {
+                    Console.WriteLine(i.EmpId + " " + i.EmpName + " " + i.EmpSal + " " + i.EmpLoc);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
